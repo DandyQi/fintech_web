e33a7f634ecb7779151e705fc03a8d6a44c4f798
qijianwei_i <qijianwei_i@didichuxing.com>
2018-07-15 04:10:47 +0800

    initial

---
+++ fintech_web.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/result')
+def result():
+    return render_template("result.html")
+
+
+@app.route('/statistics')
+def stat():
+    return render_template("stats.html")
+
+
+@app.route('/graphic')
+def graphic():
+    return render_template("graphic.html")
+
+if __name__ == '__main__':
+    app.run()
