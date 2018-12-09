e20d894a51c8733d9e05f6321da16dbe8e56655b
qijianwei <qijianwei@xiaomi.com>
2018-12-09 12:18:39 +0800

    add database

--- web.py
+++ web.py
@@ -4,12 +4,11 @@
 # Created time: 2018/12/8 14:55
 # File usage: web controller
 
-from flask import Flask
+from flask import Flask, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import render_template
 
 import ConfigParser
-
 
 cf = ConfigParser.ConfigParser()
 cf.read("config.conf")
@@ -41,18 +40,42 @@ class Entity(db.Model):
                                     self.pos)
 
 
+class Relation(db.Model):
+    __tablename__ = 'relation'
+
+    id = db.Column('id', db.Integer, primary_key=True)
+    category = db.Column('category', db.VARCHAR)
+    token = db.Column('token', db.VARCHAR)
+    synonym = db.Column('synonym', db.VARCHAR)
+    norm_token = db.Column('norm_token', db.VARCHAR)
+    extra = db.Column('extra', db.VARCHAR)
+    pos = db.Column('pos', db.VARCHAR)
+
+    def __repr__(self):
+        return '<relation {}>'.format(self.id, self.token, self.category, self.synonym, self.norm_token, self.extra,
+                                      self.pos)
+
+
 db.create_all()
 
 
-@app.route('/index')
-def index():
+@app.route("/entity", methods=['GET'])
+def entity():
+    page = request.args.get('page', 1, type=int)
     context = {
-        'entity': Entity.query.all()
+        'entity': Entity.query.paginate(page, 20, False)
     }
-    return render_template("index.html", **context)
+    return render_template("entity.html", **context)
+
+
+@app.route("/relation", methods=['GET'])
+def relation():
+    page = request.args.get('page', 1, type=int)
+    context = {
+        'relation': Relation.query.paginate(page, 20, False)
+    }
+    return render_template("relation.html", **context)
 
 
 if __name__ == '__main__':
-    e = Entity()
-    print(e.query.first().token)
     app.run()
